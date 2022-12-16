ac688dface7e426b4fd5132b527ad293c030085e
Lukas Nagel <lukas@lna-dev.net>
2022-12-16 14:12:17 +0100

    Added TestUser2

--- ActivityPubServer/Repositories/InMemRepository.cs
+++ ActivityPubServer/Repositories/InMemRepository.cs
@@ -34,6 +34,32 @@ public class InMemRepository : IInMemRepository
                 vwIDAQAB
                 -----END PUBLIC KEY-----"
                 }
+            },
+            new()
+            {
+                Context = new[]
+                {
+                    "https://www.w3.org/ns/activitystreams",
+                    "https://w3id.org/security/v1"
+                },
+                Id = new Uri("https://ap.lna-dev.net/actor/018E5972-9C9F-417B-A0CF-E8290904F737"),
+                Type = "Person",
+                PreferredUsername = "TestUser2",
+                Inbox = new Uri("https://ap.lna-dev.net/inbox"),
+                PublicKey = new()
+                {
+                    Id = new Uri("https://ap.lna-dev.net/actor#main-key"),
+                    Owner = new Uri($"https://ap.lna-dev.net/actor/018E5972-9C9F-417B-A0CF-E8290904F737"),
+                    PublicKeyPem = @"-----BEGIN PUBLIC KEY-----
+                MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAz/fLWo1xQpjFmcJrxm1c
+                /5OVC5n9pudQUiFuFeOlYhdpVmLFZtVYOCJ/fAg3vsz+pRQH9BVQoAfVHME/Js8i
+                IC/1xPyAXaqz+ZxEquL1jFGjI829u2irps/qihXqUzr9j8nd3VgTvy6mQRyZLQ8U
+                Dj16ZAcRK0NfIts9nySe7lp0R695b4TUMD/bx2wl2qT0et0puye0jeWfh3F7cwD5
+                pciRrAtWVAbe//4RvhVR8I+3H4ue15fpZNO4C7TNk12O+XYjf0pznIZ/hPfMhEYF
+                stpraIb3AI4oKgehJBa26JfGO/2ruG3GL1ZF+jg6gjGG0pojBtSUbGElVO1UAg3P
+                vwIDAQAB
+                -----END PUBLIC KEY-----"
+                }
             }
         };
 
@@ -54,6 +80,19 @@ public class InMemRepository : IInMemRepository
                         Rel = "self",
                         Type = "application/activity+json",
                         Href = new Uri("https://ap.lna-dev.net/actor/037AA32E-0A2F-4785-BC64-4C079E6557C5")
+                    }
+                }
+            },
+            new Webfinger()
+            {
+                Subject = "acct:TestUser2@ap.lna-dev.net",
+                Links = new []
+                {
+                    new Link()
+                    {
+                        Rel = "self",
+                        Type = "application/activity+json",
+                        Href = new Uri("https://ap.lna-dev.net/actor/018E5972-9C9F-417B-A0CF-E8290904F737")
                     }
                 }
             }
