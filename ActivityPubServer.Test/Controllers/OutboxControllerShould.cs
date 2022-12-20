8871e57b7e5a713bcbd88bbe3487714ec32fd80c
Lukas Nagel <lukas@lna-dev.net>
2022-12-20 13:01:01 +0100

    Updated Test

--- ActivityPubServer.Test/Controllers/OutboxControllerShould.cs
+++ ActivityPubServer.Test/Controllers/OutboxControllerShould.cs
@@ -1,3 +1,8 @@
+using ActivityPubServer.Controllers;
+using ActivityPubServer.Interfaces;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Serilog;
 using Xunit;
 
 namespace ActivityPubServer.Test.Controllers;
@@ -8,6 +13,9 @@ public class OutboxControllerShould
     public void CreatePost()
     {
         // Arrange
+        //
+        // var repository = new Mock<IMongoDbRepository>();
+        // OutboxController controller = new OutboxController(logger, repository);
 
         // Act
 
