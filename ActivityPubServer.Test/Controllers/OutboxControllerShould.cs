ecbc95c0387f4c4deec3b1bc94b78a7b230e707c
Lukas Nagel <lukas@lna-dev.net>
2022-12-20 12:50:41 +0100

    Added Test Project

---
+++ ActivityPubServer.Test/Controllers/OutboxControllerShould.cs
@@ -0,0 +1,16 @@
+using Xunit;
+
+namespace ActivityPubServer.Test.Controllers;
+
+public class OutboxControllerShould
+{
+    [Fact]
+    public void CreatePost()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
