1c5b161b9a24a4809ea6329214589c469d85b8c2
Lukas Nagel <lukas@lna-dev.net>
2023-01-11 06:43:23 +0100

    🐛 Added Json Model

--- ActivityPubServer/Model/DTOs/ReturnApplicationDto.cs
+++ ActivityPubServer/Model/DTOs/ReturnApplicationDto.cs
@@ -1,14 +1,21 @@
+using System.Text.Json.Serialization;
+
 namespace ActivityPubServer.Model.DTOs;
 
 public class ReturnApplicationDto
 {
+    [JsonPropertyName("client_id")]
     public string ClientId { get; set; }
 
+    [JsonPropertyName("client_secret")]
     public string? ClientSecret { get; set; }
 
+    [JsonPropertyName("redirect_uri")]
     public Uri RedirectUri { get; set; }
 
+    [JsonPropertyName("website")]
     public Uri? Website { get; set; }
 
+    [JsonPropertyName("name")]
     public string Name { get; set; }
 }
