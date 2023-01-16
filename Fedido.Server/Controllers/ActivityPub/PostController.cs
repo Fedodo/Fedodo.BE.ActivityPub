d20ba56cfe59016ef2d797f540366fba7a177648
Lukas Nagel <lukas@lna-dev.net>
2023-01-16 06:57:56 +0100

    🚧 Post Controller init

---
+++ Fedido.Server/Controllers/ActivityPub/PostController.cs
@@ -0,0 +1,33 @@
+using Fedido.Server.Interfaces;
+using Fedido.Server.Model.ActivityPub;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace Fedido.Server.Controllers.ActivityPub;
+
+[Route("Posts")]
+public class PostController : ControllerBase
+{
+    private readonly ILogger<PostController> _logger;
+    private readonly IMongoDbRepository _repository;
+
+    public PostController(ILogger<PostController> logger, IMongoDbRepository repository)
+    {
+        _logger = logger;
+        _repository = repository;
+    }
+    
+    // [HttpGet("{postId:guid}")]
+    // public async Task<ActionResult> GetPost(Guid postId)
+    // {
+    //     var postDefinitionBuilder = Builders<Post>.Filter;
+    //     var postFilter = postDefinitionBuilder.Eq(i => i.Id, postId);
+    //     
+    //     var post = await _repository.GetSpecificItem(postFilter, "Posts", );
+    //
+    //     if (post.To.IsPublic)
+    //     {
+    //         
+    //     }
+    // }
+}
