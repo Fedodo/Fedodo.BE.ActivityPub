93b5132f9455b6ebcd2e71dcd97b8b790838b0f5
Lukas Nagel <lukas@lna-dev.net>
2023-01-22 13:57:14 +0100

    ✅ Updated SharedInboxHandler Tests

--- Fedido.Server.Test/Handlers/KnownSharedInboxHandlerShould.cs
+++ Fedido.Server.Test/Handlers/KnownSharedInboxHandlerShould.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Collections.Generic;
 using Fedido.Server.Handlers;
 using Fedido.Server.Interfaces;
+using Fedido.Server.Model.Helpers;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Xunit;
@@ -14,17 +17,36 @@ public class KnownSharedInboxHandlerShould
     {
         var logger = new Mock<ILogger<KnownSharedInboxHandler>>();
         var repository = new Mock<IMongoDbRepository>();
-        
+
+        var sharedInboxes = new List<SharedInbox>()
+        {
+            new()
+            {
+                Id = new Guid("3F155F70-A64B-4F7E-A410-F6E7E861C9FA"),
+                SharedInboxUri = new Uri("https://example.com/inbox")
+            },
+            new()
+            {
+                Id = new Guid("3EBBB5F7-E9F0-475A-87FA-D3EA93408345"),
+                SharedInboxUri = new Uri("https://example.com/inbox")
+            }
+        };
+
+        // repository.Setup(i => i.GetSpecificItems())
+
+        repository.Setup(i => i.GetAll<SharedInbox>(DatabaseLocations.KnownSharedInbox.Database,
+            DatabaseLocations.KnownSharedInbox.Collection)).ReturnsAsync(sharedInboxes);
+
         _handler = new KnownSharedInboxHandler(logger.Object, repository.Object);
     }
-    
+
     [Fact]
     public void AddSharedInbox()
     {
         // Arrange
-        
+
         // Act
-        
+
         // Assert
     }
 }
