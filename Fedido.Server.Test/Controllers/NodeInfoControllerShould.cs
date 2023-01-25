0f3fa0cdb50855145577f31e14b368ee9584e0d7
Lukas Nagel <lukas@lna-dev.net>
2023-01-25 12:57:11 +0100

    ✅ Updated Test Class
    #102

---
+++ Fedido.Server.Test/Controllers/NodeInfoControllerShould.cs
@@ -0,0 +1,34 @@
+using Fedido.Server.Controllers;
+using Fedido.Server.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace Fedido.Server.Test.Controllers;
+
+public class NodeInfoControllerShould
+{
+    private readonly NodeInfoController _nodeInfoController;
+
+    public NodeInfoControllerShould()
+    {
+        var logger = new Mock<ILogger<NodeInfoController>>();
+        var repository = new Mock<IMongoDbRepository>();
+
+        _nodeInfoController = new NodeInfoController(logger.Object, repository.Object);
+    }
+    
+    // [Fact]
+    // public void GetNodeInfoLink()
+    // {
+    //     // Arrange
+    //     
+    //     // Act
+    //     var result = _nodeInfoController.GetNodeInfoLink();
+    //
+    //     // Assert
+    //     result.Result.ShouldBeOfType<OkObjectResult>();
+    // }
+}
